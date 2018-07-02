78db08378ed4ba149a7b6f8ae0907df385cd47dc
Vladyslav Bilyi <beliyvladislav@gmail.com>
2018-07-02 19:44:12 +0300

    extend request class

--- src/Core/Http/Request.php
+++ src/Core/Http/Request.php
@@ -18,10 +18,53 @@ class Request
      */
     private $method;
 
+    /**
+     * @var array
+     */
+    private $request;
+
+    /**
+     * @var array
+     */
+    private $query;
+
+    /**
+     * @var array
+     */
+    private $body;
+
     public function __construct()
     {
         $this->path = $_SERVER['REQUEST_URI'];
         $this->method = $_SERVER['REQUEST_METHOD'];
+        $this->request = $_REQUEST;
+        $this->query = $_GET;
+        $this->body = $_POST;
+    }
+
+    /**
+     * @param string $name
+     * @return mixed|null
+     */
+    public function get(string $name)
+    {
+        return $this->request[$name] ?? null;
+    }
+
+    /**
+     * @return array
+     */
+    public function getQuery(): array
+    {
+        return $this->query;
+    }
+
+    /**
+     * @return array
+     */
+    public function getBody(): array
+    {
+        return $this->body;
     }
 
     /**
